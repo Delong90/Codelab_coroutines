d862263749b68ccc8769ecce08b661bdfab049a2
Delong90 <jeka0307@mail.ru>
2021-07-15 10:19:24 +0300

    init

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -19,8 +19,11 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.BACKGROUND
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
@@ -98,14 +101,25 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
     }
 
     /**
-     * Wait one second then update the tap count.
+     * Wait one second then display a snackbar.
      */
-    private fun updateTaps() {
-        // TODO: Convert updateTaps to use coroutines
-        tapCount++
-        BACKGROUND.submit {
-            Thread.sleep(1_000)
-            _taps.postValue("${tapCount} taps")
+//   1 viewModelScope. launchзапустит сопрограмму в viewModelScope. Это означает, что когда задание, которому мы передали,
+//   viewModelScopeбудет отменено, все сопрограммы в этом задании / области будут отменены. Если пользователь покинул Activity
+//   до delayвозврата, эта сопрограмма будет автоматически отменена при onClearedвызове после уничтожения ViewModel.
+//   2 Поскольку viewModelScopeпо умолчанию установлен диспетчер Dispatchers.Main, эта сопрограмма будет запущена в основном потоке.
+//   Позже мы увидим, как использовать разные потоки.
+//   3 Функция delayесть suspendфункция. В Android Studio это отображается 716807c07961aacd.pngзначком в левом желобе. Несмотря на то,
+//   что эта сопрограмма работает в основном потоке, delayне блокирует поток ни на секунду. Вместо этого диспетчер планирует возобновить
+//   сопрограмму через одну секунду при следующем операторе.
+    fun updateTaps() {
+        // launch a coroutine in viewModelScope
+        viewModelScope.launch {
+            tapCount++
+            // suspend this coroutine for one second
+            delay(1_000)
+            // resume in the main dispatcher
+            // _snackbar.value can be called directly from main thread
+            _taps.postValue("$tapCount taps")
         }
     }
 
--- coroutines-codelab/start/src/test/java/com/example/android/kotlincoroutines/main/MainViewModelTest.kt
+++ coroutines-codelab/start/src/test/java/com/example/android/kotlincoroutines/main/MainViewModelTest.kt
@@ -20,9 +20,19 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.android.kotlincoroutines.fakes.MainNetworkFake
 import com.example.android.kotlincoroutines.fakes.TitleDaoFake
 import com.example.android.kotlincoroutines.main.utils.MainCoroutineScopeRule
+import com.example.android.kotlincoroutines.main.utils.getValueForTest
+import com.google.common.truth.Truth
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+
+
+
+//1) InstantTaskExecutorRuleэто правило JUnit, которое настраивает LiveData синхронное выполнение каждой задачи
+//2) MainCoroutineScopeRule- это настраиваемое правило в этой кодовой базе, которое настраивается Dispatchers.
+// Main на использование TestCoroutineDispatcherfrom kotlinx-coroutines-test. Это позволяет тестам продвигать
+// виртуальные часы для тестирования и позволяет использовать код Dispatchers.Main в модульных тестах.
+
 
 class MainViewModelTest {
     @get:Rule
@@ -43,6 +53,22 @@ class MainViewModelTest {
 
     @Test
     fun whenMainClicked_updatesTaps() {
-        // TODO: Write this
+        subject.onMainViewClicked()
+        Truth.assertThat(subject.taps.getValueForTest()).isEqualTo("0 taps")
+        coroutineScope.advanceTimeBy(1000)
+        Truth.assertThat(subject.taps.getValueForTest()).isEqualTo("1 taps")
     }
+
+//    При вызове onMainViewClickedбудет запущена только что созданная сопрограмма. Этот тест проверяет, что текст нажатий
+//    остается «0 нажатий» сразу после onMainViewClickedвызова, а затем через 1 секунду он обновляется до «1 нажатий» .
+//
+//    Этот тест использует виртуальное время для управления выполнением сопрограммы, запущенной onMainViewClicked.
+//    MainCoroutineScopeRuleПозволяет приостановить, возобновить или контролировать выполнение сопрограмм, которые
+//    запускаются на Dispatchers.Main. Здесь мы вызываем, advanceTimeBy(1_000)что заставит главного диспетчера немедленно
+//    выполнить сопрограммы, возобновление которых запланировано на 1 секунду позже.
+//
+//    Этот тест полностью детерминирован, что означает, что он всегда будет выполняться одинаково. И поскольку он
+//    полностью контролирует выполнение запускаемых сопрограмм, Dispatchers.Mainему не нужно ждать одну секунду, пока
+//    значение будет установлено.
+
 }
